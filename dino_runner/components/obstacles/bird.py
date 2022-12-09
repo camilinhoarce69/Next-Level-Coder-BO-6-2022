7fa7e06328597f06f73742d44449dc34673ee0b8
camilinhoarce69 <camilinhoarce69@gmail.com>
2022-12-09 00:57:12 -0400

    add file(bird.py, fixes conflicts)

---
+++ dino_runner/components/obstacles/bird.py
@@ -0,0 +1,9 @@
+from dino_runner.components.obstacles.obstacle import Obstacle
+import random
+
+class Bird(Obstacle):
+
+    def __init__(self, image):
+        self.type = random.randint(0, 1)
+        super().__init__(image, self.type)
+        self.rect.y = 200
