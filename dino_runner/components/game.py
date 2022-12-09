d710f3368e63c2e4f269fde8ec783236c63569cd
camilinhoarce69 <camilinhoarce69@gmail.com>
2022-12-09 00:31:45 -0400

    clase 4, changes in dinosaur.py, add files(power ups and obstacles)

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -2,6 +2,8 @@ import pygame
 
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
 from dino_runner.components.dinosaur import Dinosaur
+from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
+from dino_runner.components.power_ups.power_up_manager import PowerUpManager
 
 class Game:
     def __init__(self):
@@ -16,6 +18,9 @@ class Game:
         self.y_pos_bg = 380
         #crear dino
         self.player = Dinosaur()
+        self.obstacle_manager = ObstacleManager()
+        self.points = 0
+        self.power_up_manager = PowerUpManager()
 
     def run(self):
         # Game loop: events - update - draw
@@ -34,15 +39,26 @@ class Game:
     def update(self):
         user_input = pygame.key.get_pressed()
         self.player.update(user_input)
+        self.obstacle_manager.update(self.game_speed, self)
+        self.power_up_manager.update(self.points, self.game_speed, self.player)
+        self.score()
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
         self.player.draw(self.screen)
+        self.obstacle_manager.draw(self.screen)
+        self.power_up_manager.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
+    def score(self):
+        self.points += 1
+        if self.points % 100 == 0:
+            self.game_speed += 1
+            print(self.points)
+        self.player.check_invincibility()
 
     def draw_background(self):
         image_width = BG.get_width()
